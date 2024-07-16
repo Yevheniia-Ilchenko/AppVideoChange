a9e8314d9f75f250391d92768e373173816774c1
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-07-16 18:39:13 +0300

    Add basic fileds

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import streamlit as st
 import os
 
-st.text("Hello Streamlit")
+
+# def main():
+st.title("🎬✂️AppVideoChange✂️🎬")
+st.file_uploader("Upload video", type=["mp4", "mov", "avi"])
+st.number_input("Number of clips", min_value=1, step=1)
+st.text_input("Prompt for audio")
+st.number_input("Clip index for audio", min_value=1, step=1)
+st.number_input("Number of columns to display clips", min_value=1, step=1)
